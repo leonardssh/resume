99aea0b90a0c57048c86bd2b28c9f7220e2aef1d
Narcis B <leonardssh22@gmail.com>
2022-07-27 23:52:01 +0200

    fix resume & add page breakpoint

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { provide, readonly, Ref } from "vue";
+import { provide } from "vue";
 
 import { useHead } from "@vueuse/head";
 import { useFetch } from "@vueuse/core";
@@ -7,7 +7,6 @@ import { useFetch } from "@vueuse/core";
 import Me from "./component/Me.vue";
 import About from "./component/About.vue";
 import Experience from "./component/Experience.vue";
-import Education from "./component/Education.vue";
 import Certifications from "./component/Certifications.vue";
 import Skills from "./component/Skills.vue";
 import Languages from "./component/Languages.vue";
@@ -31,14 +30,13 @@ useHead({
 </script>
 
 <template>
-  <div class="min-h-fit">
+  <div class="page break">
     <div v-if="resume" class="flex flex-row overflow-hidden bg-effect">
       <div class="container w-[70%]">
         <Me />
         <About />
         <Experience />
         <Certifications />
-        <!-- <Education /> -->
         <Online />
       </div>
       <div
@@ -60,5 +58,22 @@ useHead({
       rgba(255, 255, 255, 0) 100%
     )
     repeat scroll 0% 0%;
+}
+
+.page {
+  width: 210mm;
+  min-height: 297mm;
+
+  @apply relative z-50 grid shadow;
+  @apply print:shadow-none;
+
+  &.break::after {
+    content: "A4 Page Break";
+    top: calc(297mm - 19px);
+
+    @apply absolute w-full border-b border-dashed border-white;
+    @apply flex items-end justify-end pr-2 pb-0.5 font-inter text-xs font-medium text-white;
+    @apply print:hidden;
+  }
 }
 </style>
--- src/component/Online.vue
+++ src/component/Online.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-center mt-[52px] pb-16">
+  <div class="flex items-center justify-center mt-[52px] pb-8">
     <a
       href="https://leonard.sh"
       class="text-xs font-inter font-normal border-b pb-[0.5px] border-dashed border-black"
